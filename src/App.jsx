11938e7ff495addb5c4766c204529dda827fb6a5
Yunus Emre Karaca <yunusemrekaraca6@gmail.com>
2024-01-10 14:10:21 +0300

    ilk react proje kodları

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Products from "./components/Products/Products.jsx";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Products /> {/* burda kullandık */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Products/ProductItem.jsx
@@ -0,0 +1,23 @@
+import "./ProductItem.css";
+import ProductInfo from "./ProductInfo";
+
+const ProductItem = ({ product }) => {
+  const { imageUrl, productName, productPrice } = product;
+
+  return (
+    <div className="product-item">
+      <div className="product-image">
+        <img src={imageUrl} alt="" />
+      </div>
+      <div>
+        <ProductInfo>
+          <h2>{productName}</h2>
+          <span>{productPrice}₺</span>
+          fmjıdgkml
+        </ProductInfo>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
